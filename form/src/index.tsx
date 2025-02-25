9472c12d56d430f8e81f71f9bf4415208be1ba77
jaibhedia <jaibhedia@gmail.com>
2025-02-25 11:03:48 +0530

    feat: added polyfills

--- form/src/index.tsx
+++ form/src/index.tsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import 'core-js/features/array/flat-map'
+import 'core-js/features/map'
+import 'core-js/features/promise'
+import 'core-js/features/set'
+import 'raf/polyfill'
+import 'whatwg-fetch'
 import './index.css'
 import { createRoot } from 'react-dom/client'
 
